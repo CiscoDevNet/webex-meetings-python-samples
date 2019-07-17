8e91d665706fda661de5affbaeb6011918602025
David Staudt <dstaudt@cisco.com>
2019-07-16 23:02:53 -0500

    - Recommit updated creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,13 @@
 # Modify the below credentials to match your Webex Meeting site, host Webex ID and password
 # This sample does not support Webex or CI OAuth/SSO authentication
 
-SITENAME = 'changeme'
-WEBEXID = 'changeme'
-PASSWORD = 'changeme'
+# Credentials for use with 'legacy' non-SSO/non-OAuth sites
+WEBEXID = 'CHANGEME'  # do not include the @domain
+SITENAME = 'CHANGEME'
+PASSWORD = 'CHANGEME'
+
+# Credentials for use with Webex OAuth sites (see oauth2.py)
+SITENAME_OAUTH = 'CHANGEME'
+WEBEXID_OAUTH = 'CHANGEME'  # must include @domain
+CLIENT_ID = 'CHANGEME'
+CLIENT_SECRET = 'CHANGEME'
